d7184a383d3e9410a7fde79ac4411947e9e785a8
fujioka8700 <fujioka8700@gmail.com>
2024-01-30 19:01:48 +0900

    [update] function part 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
-void HelloWorld(int n) {
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << "[" << i << "]" << "Hello World!" << std::endl;
-    }
-}
+// 関数の前方宣言
+void HelloWorld(int n);
 
 int main(void) {
     HelloWorld(10);
     
     return 0;
+}
+
+void HelloWorld(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "[" << i << "]" << "Hello World!" << std::endl;
+    }
 }
