373298cec7c4ea2e34aa8470e8203d4900a59afe
fujioka8700 <fujioka8700@gmail.com>
2024-01-30 19:21:15 +0900

    [update] function pointer part2

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@ int Add(int x, int y) {
 }
 
 int main(int argc, char* argv[]) {
-    int (*fp)(int, int) = Add;
+    // int (*fp)(int, int) = Add;
+    auto fp = Add;
     int result = fp(3, 5);
 
     std::cout << result << std::endl;
