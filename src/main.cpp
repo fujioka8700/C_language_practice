931a636254a531f95ba28ddd0f6ff869a7aedd8d
fujioka8700 <fujioka8700@gmail.com>
2024-01-30 19:51:01 +0900

    [update] std::function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <functional>
+
+int Add(int x, int y) {
+    return x + y;
+}
 
 int main(int argc, char* argv[]) {
-    int init = 5;
+    // int (*fp)(int, int) = Add;
+    std::function<int(int, int)> f = Add;
 
-    int (*fp)(int, int) = [](int a, int b) -> int { return a * b; };
-    auto g = [&init](int a, int b) -> int { return init + a * b; };
-
-    init = 0;
-
-    int result_copy = fp(4, 6);
-    std::cout << result_copy << std::endl;
-
-    int result_ref = g(4, 6);
-    std::cout << result_ref << std::endl;
+    int result = f(3, 5);
+    std::cout << result << std::endl;
     
+    f = [](int x, int y){ return x * y; };
+    std::cout << f(4, 7) << std::endl;
+
     return 0;
 }
