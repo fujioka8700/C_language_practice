be4f9b1a10c2ae8f0524eac552763357542b5308
fujioka8700 <fujioka8700@gmail.com>
2024-01-30 14:32:53 +0900

    [update] if

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
 int main(void) {
-    int x = 123;
-    
-    /**
-     * これもコメント
-     * これもコメント
-    */
-    // コメント
-    std::cout <<  x  << std::endl;
+    int x = 7;
 
+    if (x == 5)
+    {
+        std::cout << "xは5です" << std::endl;
+    } else if (x == 6)
+    {
+        std::cout << "xは6です" << std::endl;
+    } else
+    {
+        std::cout << "xは5と6ではありません" << std::endl;
+    }
+    
     return 0;
 }
