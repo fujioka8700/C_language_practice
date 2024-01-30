e0c97669cf329138c5d8eb98ad37d5768499b621
fujioka8700 <fujioka8700@gmail.com>
2024-01-30 13:18:52 +0900

    [update] type char.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
 int main() {
-    const int x = 5;
+    // const int x = 5;
+    // constexpr int x = 5;
+    auto x = 5;
 
     x = 10;
 
     std::cout << x << std::endl;
+
+    bool ok = false;
+
+    std::cout << ok << std::endl;
+
+    char y = 'y';
+
+    std::cout << y << std::endl;
 
     return 0;
 }
