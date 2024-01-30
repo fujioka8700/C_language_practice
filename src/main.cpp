ebb5605754f4adc414d81c350a51f0bbb0ed3050
fujioka8700 <fujioka8700@gmail.com>
2024-01-30 14:37:37 +0900

    [update] switch

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,17 @@
 int main(void) {
     int x = 7;
 
-    if (x == 5)
+    switch (x)
     {
-        std::cout << "xは5です" << std::endl;
-    } else if (x == 6)
-    {
-        std::cout << "xは6です" << std::endl;
-    } else
-    {
-        std::cout << "xは5と6ではありません" << std::endl;
+    case 5:
+        std::cout << "5です" << std::endl;
+        break;
+    case 6:
+        std::cout << "6です" << std::endl;
+        break;
+    default:
+        std::cout << "5と6以外です" << std::endl;
+        break;
     }
     
     return 0;
