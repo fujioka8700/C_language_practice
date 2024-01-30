a75caa26ced9e0796f9d6d64e7d736b227ca6a47
fujioka8700 <fujioka8700@gmail.com>
2024-01-30 13:59:34 +0900

    [update] literal

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
 
 int main(void) {
-    enum class Day {
-        Sun,
-        Mon,
-        Tue,
-        Wed,
-        Thu,
-        Fri,
-        Sat
-    };
-
-    // Day day = Wed;
-    Day day = Day::Thu;
-
-    std::cout << static_cast<int>(day) << std::endl;
+    std::cout << 26l << std::endl;
 
     return 0;
 }
