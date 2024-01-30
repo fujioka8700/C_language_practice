95a6f153867cd66464770defa1f376d0b56602e6
fujioka8700 <fujioka8700@gmail.com>
2024-01-30 14:13:33 +0900

    [update] dereference

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
 int main(void) {
-    int x = 5;
+    int  x =  5;
+    int* p = &x;
+    int  y = *p;
 
-    std::cout << x << std::cout;
+    x = 10;
+
+    std::cout << "x=" <<  x << std::endl;
+    std::cout << "p=" << *p << std::endl;
+    std::cout << "y=" <<  y << std::endl;
 
     return 0;
 }
