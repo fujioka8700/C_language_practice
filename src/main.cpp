4ac7c4695abbb5aa2ce9c0380dc69f4814c57eba
fujioka8700 <fujioka8700@gmail.com>
2024-01-30 19:00:22 +0900

    [update] function

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <bitset>
+
+void HelloWorld(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "[" << i << "]" << "Hello World!" << std::endl;
+    }
+}
 
 int main(void) {
-    unsigned int a = 0x0000000f;
-
-    std::cout << std::showbase << std::hex;
-    std::cout << ~a << std::endl;
-    std::cout << ~(a << 2) << std::endl;
+    HelloWorld(10);
     
     return 0;
 }
