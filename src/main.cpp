cf2b4fd5d6557486da0b8f43d6b6326d03e61989
fujioka8700 <fujioka8700@gmail.com>
2024-01-31 14:27:45 +0900

    [update] std::pair

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,12 @@
 #include <tuple>
 
 int main(int argc, char* argv[]) {
-    std::tuple<std::string, int, std::string>person = { "Bob", 20, "USA" };
+    // std::tuple<std::string, int, std::string>person = { "Bob", 20, "USA" };
 
-    // std::cout << std::get<0>(person) << std::endl;
+    std::pair<std::string, int> person { "Bob", 20 };
 
-    std::string name;
-    int age;
-    std::string country;
-
-    std::tie(name, age, country) = person;
-
-    std::cout << name << std::endl;
-    std::cout << age << std::endl;
-    std::cout << country << std::endl;
-
-    // std::cout << std::get<0>()
+    std::cout << std::get<1>(person) << std::endl;
+    std::cout << person.first << std::endl;
     
     std::cout << "おわり" << std::endl;
 
