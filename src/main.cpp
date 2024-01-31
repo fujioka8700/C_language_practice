ac7693cec1d50fd6237f6071d35864d9a055dbc3
fujioka8700 <fujioka8700@gmail.com>
2024-01-31 15:09:35 +0900

    [update] std::vector<int>::iterator

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@
 int main(int argc, char* argv[]) {
     std::vector<int> x = {0, 1, 2, 3};
     
-    x[2] = 100;
+    // auto it = x.begin();
+    std::vector<int>::iterator it = x.begin();
+    
+    ++it;
 
-    std::cout << x[0] << std::endl;
-    std::cout << x.size() << std::endl;
+    std::cout << *(it+2) << std::endl;
     
     std::cout << "おわり" << std::endl;
 
