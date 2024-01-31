7a4ca7c1e753dc7bd4dfbf0f14eb51b5e21cbd8c
fujioka8700 <fujioka8700@gmail.com>
2024-01-31 12:17:56 +0900

    [update] pass array to function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 
+void PringArray1(const int x[5]) {
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << x[i] << std::endl;
+    }
+    
+    std::cout << "PringArray1 end" << std::endl;
+}
+
+void PringArray2(const int* p) {
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << *(p+i) << std::endl;
+    }
+    
+    std::cout << "PringArray2 end" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
     int x[] = {2, 4, 6, 8, 10};
 
-    int* p = x;
+    PringArray1(x);
+    PringArray2(x);
 
-    std::cout << *p     << std::endl;
-    std::cout << *(p+2) << std::endl;
-    std::cout << *(p+4) << std::endl;
+    std::cout << "おわり" << std::endl;
 
     return 0;
 }
