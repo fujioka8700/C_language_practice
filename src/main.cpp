a4bb22c9446e9d42497a9ef36087084f855d5e9b
fujioka8700 <fujioka8700@gmail.com>
2024-01-31 14:02:06 +0900

    [update] std::string

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
-#include <array>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    std::array<int, 5>x = {2, 4, 6, 8, 10};
+    string s = "Hello";
 
-    cout << x[4] << endl;
-
-    // int arr1[3] = {1,2,3};
-    // array<int,3>y = {100, 200, 300};
-    array<int, 5>y = x;
-
-    cout << y[1] << endl;
+    cout << s[0] << endl;
+    cout << s.size() << endl;
     
     cout << "おわり" << endl;
 
