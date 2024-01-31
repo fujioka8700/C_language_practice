aa6381dee9f1e5d0c84a4ddc8ccfe09dfefcb3b4
fujioka8700 <fujioka8700@gmail.com>
2024-01-31 14:23:15 +0900

    [update] std::tie

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,21 @@
 #include <tuple>
 
 int main(int argc, char* argv[]) {
-    std::tuple<std::string, int>person { "Bob", 20 };
+    std::tuple<std::string, int, std::string>person = { "Bob", 20, "USA" };
 
-    std::cout << std::get<0>(person) << std::endl;
+    // std::cout << std::get<0>(person) << std::endl;
+
+    std::string name;
+    int age;
+    std::string country;
+
+    std::tie(name, age, country) = person;
+
+    std::cout << name << std::endl;
+    std::cout << age << std::endl;
+    std::cout << country << std::endl;
+
+    // std::cout << std::get<0>()
     
     std::cout << "おわり" << std::endl;
 
