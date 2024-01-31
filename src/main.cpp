ed56ca702d759703231096d38ecdf1793b9a2d0f
fujioka8700 <fujioka8700@gmail.com>
2024-01-31 11:43:15 +0900

    [update] array and pointer

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    std::cout << "container" << std::endl;
+    int x[] = {2, 4, 6, 8, 10};
+
+    int* p = x;
+
+    std::cout << *p     << std::endl;
+    std::cout << *(p+2) << std::endl;
+    std::cout << *(p+4) << std::endl;
 
     return 0;
 }
