8c5ce074ac4ef309ea1c60d2768bc3224305d777
fujioka8700 <fujioka8700@gmail.com>
2024-02-01 11:16:06 +0900

    [update] block scope

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,17 @@
 #include <iostream>
 
-namespace A {
-    int count = 2;
-
-    int GetCount() {
-        return count;
-    }
-
-    namespace P {
-        int count = 8;
-
-        int GetCount() {
-        return count;
-    }   
-    }
-}
-
-namespace B {
-    int count = 4;
-}
-
 int main(int argc, char* argv[]) {
-    std::cout << A::count << std::endl;
+    int x = 5;
 
-    std::cout << A::GetCount() << std::endl;
+    std::cout << x << std::endl;
 
-    std::cout << A::P::GetCount() << std::endl;
+    {
+        int x = 10;
 
-    std::cout << B::count << std::endl;
+        std::cout << x << std::endl;
+    }
+
+    std::cout << x << std::endl;
 
     std::cout << "おわり" << std::endl;
 
