29918d6978bc27f2d30da7d4256aea54b5b5e21c
fujioka8700 <fujioka8700@gmail.com>
2024-02-01 10:16:01 +0900

    [update] file part 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <vector>
+
+void HelloWorld();
+void HelloWorld();
 
 int main(int argc, char* argv[]) {
-    std::vector<int> x = {0, 1, 2, 3};
-    
-    // auto it = x.begin();
-    std::vector<int>::iterator it = x.begin();
-    
-    ++it;
+    HelloWorld();
 
-    std::cout << *(it+2) << std::endl;
-    
     std::cout << "おわり" << std::endl;
 
     return 0;
+}
+
+void HelloWorld() {
+    std::cout << "HelloWorld" << std::endl;
 }
