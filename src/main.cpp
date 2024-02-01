def0f593ae08614edb23ce85eb8f6590600f934e
fujioka8700 <fujioka8700@gmail.com>
2024-02-01 13:51:28 +0900

    [update] internal linkage.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
 #include "aaa.hpp"
 
-#ifndef HOGE
-#define HOGE
-int hoge = 5;
-#endif
-#ifndef HOGE
-#define HOGE
-int hoge = 5;
-#endif
-
 int main(int argc, char* argv[]) {
     // std::cout << ::x << std::endl;
     DoSometing();
 
-    std::cout << hoge << std::endl;
+    extern int x;
+
+    std::cout << x << std::endl;
 
     std::cout << "おわり" << std::endl;
 
