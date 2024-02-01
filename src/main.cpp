1fe0644796bce4de8ce103aa1cbe4f03737aae28
fujioka8700 <fujioka8700@gmail.com>
2024-02-01 10:20:47 +0900

    [update] file part 3

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,18 @@ void HelloWorld();
 int main(int argc, char* argv[]) {
     HelloWorld();
 
+    int x;
+    // int x;
+
+    std::cout << x << std::endl;
+
     std::cout << "おわり" << std::endl;
 
     return 0;
+}
+
+void HelloWorld() {
+    std::cout << "HelloWorld" << std::endl;
 }
 
 void HelloWorld() {
