cc63b66d1b54eeac6b0de2b0158d496fd0857188
fujioka8700 <fujioka8700@gmail.com>
2024-02-03 13:26:41 +0900

    [update] std::ofstream part2

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 int main(int argc, char* argv[]) {
-  std::ofstream file("../files/fruits.txt");
+  std::ofstream file("../files/fruits.txt", std::ios::app);
   std::vector<std::string> fruits = { "apple", "strawberry", "pear", "grape" };
 
   for (const auto &fruit: fruits)
