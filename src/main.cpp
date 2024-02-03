0a2cee201ea43c9472adb6e7511534febe42f772
fujioka8700 <fujioka8700@gmail.com>
2024-02-03 11:28:03 +0900

    [update] std::ifstream

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,17 @@
 #include "aaa.hpp"
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <ctype.h>
 
 int main(int argc, char* argv[]) {
-  std::string str;
+  std::ifstream file("../files/file.txt");
+  std::string line;
 
-  std::cout << "整数を入力してください: ";
-
-  std::cin >> str;
-
-  for (int i = 0; i < (int)str.length(); i++)
+  while (std::getline(file, line))
   {
-    bool isNumber = false;
-
-    isNumber = isdigit(str[i]);
-
-    if (!(i == (int)str.length() - 1))
-    {
-      continue;
-    }
-
-    if (isNumber) {
-      std::cout << "入力した整数は " << str << " です。" << std::endl;
-    } else {
-      std::cout << "不正な入力です。" << std::endl;
-    }
-
-    if (!isNumber)
-    {
-      break;
-    }
-    
+    std::cout << line << std::endl;
   }
-
+  
   std::cout << "おわり" << std::endl;
 
   return 0;
