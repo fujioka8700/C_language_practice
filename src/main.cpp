6cda4462b569e8036045893d522738f1e342515b
fujioka8700 <fujioka8700@gmail.com>
2024-02-03 11:15:19 +0900

    [update] std::cin

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
 #include "aaa.hpp"
 #include <iostream>
 #include <string>
+#include <ctype.h>
 
 int main(int argc, char* argv[]) {
-  std::cout << "好きな食べ物を入力してください：" << std::endl;
+  std::string str;
 
-  std::string food;
+  std::cout << "整数を入力してください: ";
 
-  std::cin >> food;
+  std::cin >> str;
 
-  std::cout << "好きな食べ物は" << food << "です。" << std::endl;
+  for (int i = 0; i < (int)str.length(); i++)
+  {
+    bool isNumber = false;
+
+    isNumber = isdigit(str[i]);
+
+    if (!(i == (int)str.length() - 1))
+    {
+      continue;
+    }
+
+    if (isNumber) {
+      std::cout << "入力した整数は " << str << " です。" << std::endl;
+    } else {
+      std::cout << "不正な入力です。" << std::endl;
+    }
+
+    if (!isNumber)
+    {
+      break;
+    }
+    
+  }
 
   std::cout << "おわり" << std::endl;
 
