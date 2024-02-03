c3f2cad40a0a6711812d8a3901ff9933db4ddb71
fujioka8700 <fujioka8700@gmail.com>
2024-02-03 13:49:23 +0900

    [update] delete

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@ int main(int argc, char* argv[]) {
   int* p1 = new int(100);
   int* p2 = new int[5];
 
+  delete p1;
+  delete[] p2;
+
+  std::cout << "*p1=" << *p1 << std::endl;
+  std::cout << "*p2=" << *p2 << std::endl;
+
   std::cout << "おわり" << std::endl;
 
   return 0;
