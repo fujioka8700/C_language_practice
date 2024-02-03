b2db1ad8017f369fac0617efc00c8ec86e175d0e
fujioka8700 <fujioka8700@gmail.com>
2024-02-03 14:08:03 +0900

    [update] function arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include "aaa.hpp"
 #include <iostream>
 
+void changeToFifty1(int v) {
+  v = 50;
+}
+
+void changeToFifty2(int* v) {
+  *v = 50;
+}
+
 int main(int argc, char* argv[]) {
-  int* x = new int(100);
-  int* y = x;
+  int a = 100;
+  changeToFifty1(a);
+  std::cout << a << std::endl;
 
-  std::cout << x << '=' << *x << std::endl;
-  std::cout << y << '=' << *y << std::endl;
-
-  *y = 50;
-
-  std::cout << x << '=' << *x << std::endl;
-  std::cout << y << '=' << *y << std::endl;
+  int b = 100;
+  changeToFifty2(&b);
+  std::cout << b << std::endl;
 
   std::cout << "おわり" << std::endl;
 
