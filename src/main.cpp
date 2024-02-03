005af4e7b58a02b34b9a4fe9e4058cdd16244b11
fujioka8700 <fujioka8700@gmail.com>
2024-02-03 14:00:58 +0900

    [update] shallow copy

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,16 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-  // deep copy
-  int x = 100;
-  int y = x;
+  int* x = new int(100);
+  int* y = x;
 
-  y = 50;
+  std::cout << x << '=' << *x << std::endl;
+  std::cout << y << '=' << *y << std::endl;
 
-  std::cout << x << std::endl; // x はそのまま
-  std::cout << y << std::endl;
+  *y = 50;
+
+  std::cout << x << '=' << *x << std::endl;
+  std::cout << y << '=' << *y << std::endl;
 
   std::cout << "おわり" << std::endl;
 
