ea810740c73fa6a341b416f4668417516e8909e5
fujioka8700 <fujioka8700@gmail.com>
2024-02-04 19:19:59 +0900

    [update] copy constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,17 @@
 #include <iostream>
 
-class Rectangle {
+class Movable {
   public:
-    Rectangle();
-    Rectangle(const Rectangle& r): height_(100), width_(100) {};
-
-    int Area() const {
-      return height_ * width_;
-    };
-
-  private:
-    const int height_;
-    const int width_;
-};
-
-Rectangle::Rectangle() : height_(0), width_(0) {}
-
-class Copyable {
-  public:
-    Copyable(){};
-    Copyable(const Copyable& c){};
+    Movable() {};
+    Movable(Movable&& m) {};
 };
 
 int main(int argc, char* argv[]) {
-  Rectangle r1;
-  Rectangle r2(r1);
+  Movable m1;
+  Movable m2(std::move(m1));
   
-  std::cout << r1.Area() << std::endl;
-  std::cout << r2.Area() << std::endl;
-
-  Copyable c1;
-  Copyable c2(c1);
-
-  std::cout << &c1 << std::endl;
-  std::cout << &c2 << std::endl;
+  std::cout << &m2 << std::endl;
+  std::cout << &m1 << std::endl;
 
   std::cout << "おわり" << std::endl;
 
