97f9f76fe885918254ed775b5833660b15397989
fujioka8700 <fujioka8700@gmail.com>
2024-02-04 20:23:36 +0900

    [update] extends part2

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 
-class DynamicArray {
-  public:
+class BasicArray {
+ public:
+    ~BasicArray() {
+        std::cout << "BasicArray::~BasicArray() is called." << std::endl;
+    }
+
+    virtual void Set(int index, int value) = 0;
+    virtual int Get(int index) const = 0;
+};
+
+class DynamicArray : public BasicArray {
+ public:
     DynamicArray(int size, int initial_value) {
-      data_ = new int[size];
-      size_ = size;
-
-      for (int i = 0; i < size; i++) {
-        data_[i] = initial_value;
-      }
-    }
-
-    void Set(int index, int value) {
-      data_[index] = value;
-    }
-
-    int Get(int index) const {
-      return data_[index];
+        data_ = new int[size];
+        for (auto i = 0; i < size; ++i) {
+            data_[i] = initial_value;
+        }
     }
 
     ~DynamicArray() {
-      std::cout << "DynamicArray::~DynamicArray() is called." << std::endl;
-      delete[] data_;
-
-      for (int i = 0; i < size_; i++) {
-        std::cout << data_[i] << std::endl;
-      }
+        std::cout << "DynamicArray::~DynamicArray() is called." << std::endl;
+        delete[] data_;
     }
 
-  private:
+    void Set(int index, int value) { data_[index] = value; }
+
+    int Get(int index) const { return data_[index]; }
+
+ private:
     int* data_;
-    int size_;
 };
 
 int main(int argc, char* argv[]) {
   DynamicArray d(5, 1);
+
+  std::cout << d.Get(2) << std::endl;
 
   d.Set(2,11);
 
