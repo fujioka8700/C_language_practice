01c9d3cd49f808ea645a700bf2cfbbdffd2c0717
fujioka8700 <fujioka8700@gmail.com>
2024-02-04 15:21:10 +0900

    [update] constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,21 @@
 #include <iostream>
-#include <memory>
 
 class Rectangle {
   public:
-    int Area() const {
-      return height_ * width_;
-    };
-  
-    virtual void Describe() const {
-      std::cout << "height=" << height_ << std::endl;
-      std::cout << "width=" << width_ << std::endl;
-    }
+    Rectangle(int height, int width) :
+      height_(height), width_(width) {}
 
-    int height_;
-    int width_;
-};
+    int Area() const;
 
-class Squere : public Rectangle {
-  public:
-    void setSize(int size) {
-      height_ = size;
-      width_ = size;
-    }
-
-    void Describe() const override {
-      std::cout << "size=" << height_ << std::endl;
-    }
+  private:
+    const int height_;
+    const int width_;
 };
 
 int main(int argc, char* argv[]) {
-  Squere s;
-  s.setSize(10);
-  s.Describe();
+  Rectangle r(10, 20);
 
-  Rectangle& r = s;
-  r.Describe();
+  // std::cout << r << std::endl;
 
   std::cout << "おわり" << std::endl;
 
