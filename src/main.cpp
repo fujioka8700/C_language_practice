094f1a84e94cc43b2a490f129c7aaa8e3fd7d302
fujioka8700 <fujioka8700@gmail.com>
2024-02-04 19:14:55 +0900

    [update] copy constructor

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 class Rectangle {
   public:
     Rectangle();
+    Rectangle(const Rectangle& r): height_(100), width_(100) {};
 
     int Area() const {
       return height_ * width_;
@@ -13,12 +14,26 @@ class Rectangle {
     const int width_;
 };
 
-Rectangle::Rectangle() : height_(), width_() {}
+Rectangle::Rectangle() : height_(0), width_(0) {}
+
+class Copyable {
+  public:
+    Copyable(){};
+    Copyable(const Copyable& c){};
+};
 
 int main(int argc, char* argv[]) {
-  Rectangle r;
+  Rectangle r1;
+  Rectangle r2(r1);
+  
+  std::cout << r1.Area() << std::endl;
+  std::cout << r2.Area() << std::endl;
 
-  std::cout << r.Area() << std::endl;
+  Copyable c1;
+  Copyable c2(c1);
+
+  std::cout << &c1 << std::endl;
+  std::cout << &c2 << std::endl;
 
   std::cout << "おわり" << std::endl;
 
