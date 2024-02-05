3dbb22a5928cd07376e225c11e23bd483b0e7159
fujioka8700 <fujioka8700@gmail.com>
2024-02-05 11:25:23 +0900

    [update] vector shallow copy

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 
 
 int main(int argc, char* argv[]) {
-  std::vector<int> x(10000);
-  std::vector<int> y = x;
+  std::vector<int>* x = new std::vector<int>(100);
+  std::vector<int>* y = x;
 
-  x[10] = 100;
+  (*x)[10] = 100;
 
-  std::cout << x[10] << std::endl;
-  std::cout << y[10] << std::endl;
+  std::cout << (*x)[10] << std::endl;
+  std::cout << (*y)[10] << std::endl;
 
   std::cout << "おわり" << std::endl;
 
