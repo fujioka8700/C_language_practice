0de84ff930773e6d62c05f9b689ca12089448b0e
fujioka8700 <fujioka8700@gmail.com>
2024-02-05 10:14:13 +0900

    [update] new part1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+  int* p1 = new int(100);
+
+  std::cout << *p1 << std::endl;
+  delete p1;
+  std::cout << *p1 << std::endl;
+
   std::cout << "おわり" << std::endl;
 
   return 0;
