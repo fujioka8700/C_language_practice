056dd6005189cdbe8eb1f75d3dc3bca49dba0d86
fujioka8700 <fujioka8700@gmail.com>
2024-02-05 17:58:47 +0900

    [update] 純粋仮想関数

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
-#include "rectangle.hpp"
-#include "square.hpp"
+
+class Polygon {
+  public:
+    virtual int Area() const = 0;
+};
+
+class Rectangle : public Polygon {
+  public:
+    Rectangle() : height_(2), width_(3) {}
+    int Area() const override {
+      return height_ * width_;
+    }
+
+  public:
+    int height_;
+    int width_;
+};
 
 int main(int argc, char* argv[]) {
-  Square s;
-  s.SetSize(100);
-  s.Describe();
-
-  Rectangle& r = s;
-  r.Describe();
+  Rectangle r;
   std::cout << r.Area() << std::endl;
+
+  Polygon& p = r;
+  std::cout << p.Area() << std::endl;
 
   std::cout << "おわり" << std::endl;
 
