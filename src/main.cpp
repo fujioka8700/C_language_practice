058daaa15508f1d731d5642bdb6033a008ccb01a
fujioka8700 <fujioka8700@gmail.com>
2024-02-05 11:35:41 +0900

    [update] 右辺値と左辺値

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
 
-
 int main(int argc, char* argv[]) {
-  std::vector<int>* x = new std::vector<int>(100);
-  std::vector<int>* y = x;
+  std::vector<int> x(1000000);
 
-  (*x)[10] = 100;
+  x[10] = 999;
 
-  std::cout << (*x)[10] << std::endl;
-  std::cout << (*y)[10] << std::endl;
+  std::vector<int> y = x;
+
+  std::cout << x[10] << std::endl;
+  std::cout << y[10] << std::endl;
 
   std::cout << "おわり" << std::endl;
 
