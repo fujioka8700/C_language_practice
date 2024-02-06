ea4b6055c5810a14407e561f2c73c81fa024a984
fujioka8700 <fujioka8700@gmail.com>
2024-02-06 11:07:23 +0900

    [update] 二項演算子 オーバーロード Part2

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,37 @@ Integer operator+(const Integer& lhs, const Integer& rhs) {
   return tmp;
 }
 
+class RealNumber {
+  public:
+    explicit RealNumber(double value) : value_(value) {}
+
+    double Value() const {
+      return value_;
+    }
+
+  private:
+    double value_;
+};
+
+RealNumber operator+(const Integer& lhs, const RealNumber& rhs) {
+  RealNumber tmp(lhs.Value() + rhs.Value());
+  return tmp;
+}
+
+RealNumber operator+(const RealNumber& lhs, const Integer& rhs) {
+  RealNumber tmp(lhs.Value() + rhs.Value());
+  return tmp;
+}
+
 int main(int argc, char* argv[]) {
   Integer a(2);
-  Integer b(3);
-  Integer c = a + b;
+  RealNumber b(3.5);
 
-  std::cout << a.Value() << std::endl;
-  std::cout << b.Value() << std::endl;
+  RealNumber c = a + b;
   std::cout << c.Value() << std::endl;
+  
+  RealNumber d = b + a;
+  std::cout << d.Value() << std::endl;
 
   std::cout << "おわり" << std::endl;
 
