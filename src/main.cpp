9f89d8a935b938773fb9dc889cd09fa0a27098d4
fujioka8700 <fujioka8700@gmail.com>
2024-02-06 17:37:25 +0900

    [update] 前置インクリメント

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
-#include "utils.hpp"
-#include "integer.hpp"
-#include "realnumber.hpp"
+
+class Integer {
+  public:
+    Integer(int value) : value_(value) {};
+
+  int Value() const {
+    return value_;
+  }
+
+  Integer& operator++() {
+    ++value_;
+    return *this;
+  }
+
+  Integer operator-() const  {
+    Integer tmp(-value_);
+    return tmp;
+  }
+
+  private:
+    int value_;
+};
 
 int main(int argc, char* argv[]) {
   Integer a(2);
-  Integer b(3);
-  Integer c = a + b;
-  std::cout << c.Value() << std::endl;
 
-  RealNumber d(3.5);
-  RealNumber e = a + d;
-  std::cout << e.Value() << std::endl;
+  Integer b = -a; // a.operator-();
+  std::cout << b.Value() << std::endl;
 
-  RealNumber f = d + a;
-  std::cout << f.Value() << std::endl;
+  ++a; // a.operator++();
+  std::cout << a.Value() << std::endl;
 
   std::cout << "おわり" << std::endl;
 
