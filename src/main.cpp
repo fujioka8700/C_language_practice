17fccf4a02ca1ab0fe82b0050c078264bbc5984f
fujioka8700 <fujioka8700@gmail.com>
2024-02-07 14:21:41 +0900

    [update] ムーブ代入演算子

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,15 @@ class Movable {
     Movable(Movable&& m) : value_(m.Value()) {
       std::cout << "ムーブコンストラクタ" << std::endl;
     }
+    Movable& operator=(Movable&& m) { // ムーブ代入演算子
+      std::cout << "ムーブ代入演算子" << std::endl;
+      value_ = m.Value();
+      return *this;
+
+      std::cout << Value() << std::endl;
+      std::cout << this->Value() << std::endl;
+      std::cout << (*this).Value() << std::endl;
+    }
 
     int Value() {
       return value_;
@@ -19,7 +28,8 @@ class Movable {
 
 int main(int argc, char* argv[]) {
   Movable m1(10);
-  Movable m2(std::move(m1));
+  Movable m2(20);
+  m1.operator=(std::move(m2));
 
   std::cout << m1.Value() << std::endl;
   std::cout << m2.Value() << std::endl;
