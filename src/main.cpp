361fef70b4318b8ea37190f0123462d68571c02e
fujioka8700 <fujioka8700@gmail.com>
2024-02-07 18:14:24 +0900

    [update] struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#include "brightness.hpp"
+
+struct Rectangle {
+  int height;
+  int width;
+};
 
 int main(int argc, char* argv[]) {
-  Brightness b1 = Brightness::Maximum();
-  std::cout << b1.Value() << std::endl;
+  Rectangle r;
+  r.height = 10;
+  r.width  = 20;
 
-  Brightness b2 = Brightness::Minimum();
-  std::cout << b2.Value() << std::endl;
-
-  Brightness b3 = Brightness::Median();
-  std::cout << b3.Value() << std::endl;
+  std::cout << r.height << std::endl;
+  std::cout << r.width << std::endl;
 
   std::cout << "おわり" << std::endl;
 
