dee2620e6d502ff45804cb87bb064a12bc612898
fujioka8700 <fujioka8700@gmail.com>
2024-02-07 13:49:15 +0900

    [update] std::move

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,9 @@
 #include <iostream>
-
-class Copyable {
-  public:
-    Copyable() {};
-
-    Copyable(const Copyable& c) {
-      std::cout << "c3:" << this << std::endl;
-      std::cout << "c2:" << &c << std::endl;
-      std::cout << "コピーコンストラクタ" << std::endl;
-    }
-
-    Copyable& operator=(const Copyable& c) {
-      std::cout << "c1:" << this << std::endl;
-      std::cout << "c2:" << &c << std::endl;
-      std::cout << "コピー演算子" << std::endl;;
-      return * this;
-    };
-};
+#include <vector>
 
 int main(int argc, char* argv[]) {
-  Copyable c1, c2;
-  c1 = c2; // c1.operator=(c2);
-  Copyable c3 = c2;
+  std::vector<int> x(1000000);
+  std::vector<int> y = std::move(x);
 
   std::cout << "おわり" << std::endl;
 
