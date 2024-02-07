a9932d1cf19b9799800d353cd5b98a2515a37ee2
fujioka8700 <fujioka8700@gmail.com>
2024-02-07 17:49:44 +0900

    [update] static メンバ関数

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-#include "counter.hpp"
+#include "brightness.hpp"
 
 int main(int argc, char* argv[]) {
-  std::cout << Counter::count_ << std::endl;
-  ++Counter::count_;
-  std::cout << Counter::count_ << std::endl;
+  Brightness b1 = Brightness::Maximum();
+  std::cout << b1.Value() << std::endl;
+
+  Brightness b2 = Brightness::Minimum();
+  std::cout << b2.Value() << std::endl;
+
+  Brightness b3 = Brightness::Median();
+  std::cout << b3.Value() << std::endl;
 
   std::cout << "おわり" << std::endl;
 
