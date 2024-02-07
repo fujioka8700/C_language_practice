7c48d347e760b5ad10057c9a250c6bf9ed6955c9
fujioka8700 <fujioka8700@gmail.com>
2024-02-07 19:56:37 +0900

    [update] static_cast Part2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-  double dx = 3.14;
-  int x = static_cast<int>(dx);
+enum class Day {
+    Sun,  // 0
+    Mon,  // 1
+    Tue,  // 2
+    Wed,  // 3
+    Thu,  // 4
+    Fri,  // 5
+    Sat   // 6
+};
 
-  std::cout << x << std::endl;
+int main(int argc, char* argv[]) {
+  Day day1 = static_cast<Day>(1);
+  int day2 = static_cast<int>(Day::Tue);
+  
+  std::cout << static_cast<int>(day1) << std::endl;
+  std::cout << day2 << std::endl;
 
   std::cout << "おわり" << std::endl;
 
