2cf27cba6db77acede2a9aec8378f15445fe3fcd
fujioka8700 <fujioka8700@gmail.com>
2024-02-07 14:00:20 +0900

    [update] move constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#include <vector>
+
+class Movable {
+  public:
+    Movable(int value) : value_(value) {
+      std::cout << "デフォルトコンストラクタ" << std::endl;
+    }
+    Movable(Movable&& m) : value_(m.Value()) {
+      std::cout << "ムーブコンストラクタ" << std::endl;
+    }
+
+    int Value() {
+      return value_;
+    }
+
+  private:
+    int value_;
+};
 
 int main(int argc, char* argv[]) {
-  std::vector<int> x(1000000);
-  std::vector<int> y = std::move(x);
+  Movable m1(10);
+  Movable m2(std::move(m1));
+
+  std::cout << m1.Value() << std::endl;
+  std::cout << m2.Value() << std::endl;
 
   std::cout << "おわり" << std::endl;
 
