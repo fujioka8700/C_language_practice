ff2037187ed36544d5153bbc0cbfcaced2fd4e9f
fujioka8700 <fujioka8700@gmail.com>
2024-02-09 18:00:23 +0900

    [update] 完全特殊化

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-#include "sum.hpp"
+
+template <typename T>
+T DoSomething(T a, T b) {
+  return a + b;
+}
+
+template <>
+double DoSomething<double>(double a, double b) {
+  return a * b;
+}
 
 int main(int argc, char* argv[]) {
-  std::cout << Sum<int>(1, 2) << std::endl; 
+  std::cout << DoSomething(2, 3) << std::endl;
+  std::cout << DoSomething(2.0, 3.0) << std::endl;
 
   std::cout << "おわり" << std::endl;
 
