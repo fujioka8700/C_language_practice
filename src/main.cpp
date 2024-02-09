d8ec129032862af3a09b9998eddbf09addfa3008
fujioka8700 <fujioka8700@gmail.com>
2024-02-09 18:15:46 +0900

    [update] 完全特殊化 Part2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,54 @@
 #include <iostream>
 
 template <typename T>
-T DoSomething(T a, T b) {
-  return a + b;
-}
+class Array {
+  public:
+    explicit Array(int size)
+      : size_(size),
+        data_(new T[size_]) {}
+
+    ~Array() {
+      delete[] data_;
+    }
+
+    int Size() const {
+      return size_;
+    }
+
+  private:
+    const int size_;
+    T* data_;
+};
 
 template <>
-double DoSomething<double>(double a, double b) {
-  return a * b;
-}
+class Array<bool> {
+  public:
+    explicit Array(int size)
+      : size_(size),
+        data_size_((size - 1) / 8 + 1),
+        data_(new uint8_t[data_size_]) {}
+
+    ~Array() {
+      delete[] data_;
+    }
+
+    int Size() const {
+      return size_;
+    }
+
+  public:
+    const int size_;
+    const int data_size_;
+    uint8_t* data_;
+};
 
 int main(int argc, char* argv[]) {
-  std::cout << DoSomething(2, 3) << std::endl;
-  std::cout << DoSomething(2.0, 3.0) << std::endl;
+  Array<int> ary1(5);
+  std::cout << ary1.Size() << std::endl;
+
+  Array<bool> ary2(10); // テンプレート引数を bool にした時のみ完全特殊化される。
+  std::cout << ary2.Size() << std::endl;
+  std::cout << ary2.data_size_ << std::endl;
 
   std::cout << "おわり" << std::endl;
 
