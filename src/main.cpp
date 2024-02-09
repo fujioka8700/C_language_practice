d5623aad0615b44a6556a4e7cf41371ec5550d6e
fujioka8700 <fujioka8700@gmail.com>
2024-02-09 17:20:54 +0900

    [update] 複数のテンプレート引数

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-template <typename T>
+template <typename T, typename U>
 class Rectangle {
   public:
-    Rectangle(T height, T width)
+    Rectangle(T height, U width)
       : height_(height), width_(width) {}
 
     T Area() const {
@@ -16,10 +16,10 @@ class Rectangle {
 };
 
 int main(int argc, char* argv[]) {
-  Rectangle<int> r1(10, 20);
+  Rectangle<int, int> r1(10, 20);
   std::cout << r1.Area() << std::endl;
 
-  Rectangle<double> r2(1.2, 3.4);
+  Rectangle<double, double> r2(1.2, 3.4);
   std::cout << r2.Area() << std::endl;
 
   std::cout << "おわり" << std::endl;
