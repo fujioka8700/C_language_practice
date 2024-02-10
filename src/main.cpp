147bed6c012f0754ab94642936992be84d683353
fujioka8700 <fujioka8700@gmail.com>
2024-02-10 10:00:09 +0900

    [update] const ポインタ

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,15 @@
 
 int main(int argc, char* argv[]) {
   int x = 5;
-  int* p = &x;
-  int y = *p;
+  const int* p = &x;
+
+  *p = 456;
 
   std::cout << &x << std::endl;
   std::cout <<  p << std::endl;
 
   std::cout <<  x << std::endl;
   std::cout << *p << std::endl;
-  std::cout <<  y << std::endl;
 
   std::cout << "おわり" << std::endl;
 
