1bb58ce3867668c5e36473e04bb40b910f8e620f
fujioka8700 <fujioka8700@gmail.com>
2024-02-10 20:00:21 +0900

    [update] アップキャスト

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
-#include <vector>
+
+class Rectangle {
+  public:
+    int Area() const {
+      return height_ * width_;
+    }
+
+  int height_;
+  int width_;
+};
+
+class Square : public Rectangle {
+  public:
+    void SetSize(int size) {
+      height_ = size;
+      width_ = size;
+    }
+};
 
 int main(int argc, char* argv[]) {
-  std::vector<int> x(1000000);
-  std::vector<int> y = std::move(x);
+    Square s;
+    s.SetSize(10);
 
-  std::cout << x[0] << std::endl;
-  std::cout << y[0] << std::endl;
+    const Rectangle& r = s;
+    // r.SetSize(5); 不可能
+    std::cout << "area = " << r.Area() << std::endl;
 
   std::cout << "おわり" << std::endl;
 
