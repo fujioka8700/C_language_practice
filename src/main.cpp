ccef13414ccc5dcf823d70f8600eda66144a2242
fujioka8700 <fujioka8700@gmail.com>
2024-02-10 09:58:58 +0900

    [update] デリファレンス

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,14 @@
 int main(int argc, char* argv[]) {
   int x = 5;
   int* p = &x;
+  int y = *p;
 
   std::cout << &x << std::endl;
-  std::cout << p  << std::endl;
+  std::cout <<  p << std::endl;
 
-  std::cout << x  << std::endl;
+  std::cout <<  x << std::endl;
   std::cout << *p << std::endl;
-
+  std::cout <<  y << std::endl;
 
   std::cout << "おわり" << std::endl;
 
